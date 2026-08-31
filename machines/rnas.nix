d581eeefd56fe8a3972f5ee589ded185a9299706
Daniel Bast <2790401+dbast@users.noreply.github.com>
2026-08-31 07:04:40 +0200

    Updates from rnas install (#97)

--- machines/rnas.nix
+++ machines/rnas.nix
@@ -6,6 +6,7 @@
 
   networking.hostName = "rnas";
   boot.loader.generic-extlinux-compatible.configurationLimit = 5;
+  hardware.deviceTree.name = "rockchip/rk3568-qnap-ts233-pcb-12-11.dtb";
 
   system.stateVersion = "25.11";
 }
