8f3437cbbc78171b325d8413c51e88a5e9665f84
Daniel Bast <2790401+dbast@users.noreply.github.com>
2026-08-31 11:50:57 +0200

    rnas bring up: more tools, hdd, hdd spindown, firmware (#99)

--- machines/rnas.nix
+++ machines/rnas.nix
@@ -1,4 +1,10 @@
 {
+  pkgs,
+  lib,
+  ...
+}:
+
+{
   imports = [
     ./base.nix
     ../disko/rnas.nix
@@ -7,6 +13,32 @@
   networking.hostName = "rnas";
   boot.loader.generic-extlinux-compatible.configurationLimit = 5;
   hardware.deviceTree.name = "rockchip/rk3568-qnap-ts233-pcb-12-11.dtb";
+  hardware.enableRedistributableFirmware = true;
+
+  fileSystems."/lake" = {
+    device = "/dev/disk/by-label/hdd-lake";
+    fsType = "xfs";
+    options = [
+      "defaults"
+      "logbsize=256k"
+      "X-fstrim.notrim"
+    ];
+  };
+
+  services.udev.extraRules =
+    let
+      mkRule = as: lib.concatStringsSep ", " as;
+      mkRules = rs: lib.concatStringsSep "\n" rs;
+    in
+    mkRules [
+      (mkRule [
+        ''ACTION=="add|change"''
+        ''SUBSYSTEM=="block"''
+        ''KERNEL=="sd[a-z]"''
+        ''ATTR{queue/rotational}=="1"''
+        ''RUN+="${pkgs.hdparm}/bin/hdparm -S 244 /dev/%k"''
+      ])
+    ];
 
   system.stateVersion = "25.11";
 }
